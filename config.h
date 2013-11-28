2b6b12eba1d8e7785674ca459070057d56cb840a
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2013-11-28 03:30:39 +0100

    updated config files

--- config.h
+++ config.h
@@ -51,7 +51,7 @@
 #define PACKAGE_NAME "arduserial"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "arduserial 1.5"
+#define PACKAGE_STRING "arduserial 1.6"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "arduserial"
@@ -60,10 +60,10 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.5"
+#define PACKAGE_VERSION "1.6"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.5"
+#define VERSION "1.6"
